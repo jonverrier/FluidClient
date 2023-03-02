7eba8568d21426215360e11e7e15221fcba880da
Jon <jonathan.verrier@hotmail.com>
2023-03-02 15:39:38 +0000

    Next version of text rendering - vertical centering.

--- src/TextRenderer.ts
+++ src/TextRenderer.ts
@@ -20,10 +20,13 @@ export class TextShapeRenderer extends ShapeRenderer {
 
    // http://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial/
    wrapText(context: CanvasRenderingContext2D, text: string,
-      x: number, y: number, maxWidth: number, lineHeight: number): void {
+      x: number, y: number, maxWidth: number, lineHeight: number,
+      drawText: boolean): number {
 
       var cars = text.split("\n");
       var spaceWidth = context.measureText(" ").width;
+
+      var dy = 0;
 
       for (var ii = 0; ii < cars.length; ii++) {
 
@@ -38,9 +41,11 @@ export class TextShapeRenderer extends ShapeRenderer {
             // Print the txt if we have incrementally exceeded maxWidth, 
             // or if we only have one word so we have to any way. 
             if ((testWidth > maxWidth) || ((testWidth > maxWidth) && n === 0)) {
-               context.fillText(line, x + (maxWidth - lineWidth) / 2, y);
+               if (drawText)
+                  context.fillText(line, x + (maxWidth - lineWidth) / 2, y);
                line = words[n] + " ";
                y += lineHeight;
+               dy += lineHeight;
                lineWidth = (testWidth - lineWidth) - spaceWidth / 2;
             }
             else {
@@ -52,8 +57,11 @@ export class TextShapeRenderer extends ShapeRenderer {
 
       var metrics = context.measureText(line);
       var testWidth = metrics.width;
-      context.fillText(line, x + (maxWidth - testWidth) / 2, y);
-      y += lineHeight;
+      if (drawText)
+         context.fillText(line, x + (maxWidth - testWidth) / 2, y);
+      dy += lineHeight;
+
+      return dy;
    }
 
    draw(ctx: CanvasRenderingContext2D,
@@ -63,9 +71,21 @@ export class TextShapeRenderer extends ShapeRenderer {
 
       this.setPen(ctx, shape. pen);
 
+      var dyNeeded = this.wrapText(ctx, textShape.text,
+         textShape.boundingRectangle.x,
+         textShape.boundingRectangle.y,
+         textShape.boundingRectangle.dx,
+         (ctx as any).lineHeight,           // The canvas puts this on as an extra variable
+         false);
+
+      var deltaY: number = (textShape.boundingRectangle.dy - dyNeeded) / 2;
+
       this.wrapText(ctx, textShape.text,
-         textShape.boundingRectangle.x, textShape.boundingRectangle.y, textShape.boundingRectangle.dx,
-         (ctx as any).lineHeight); // The canvas puts this on as an extra variable
+         textShape.boundingRectangle.x,
+         textShape.boundingRectangle.y + deltaY,
+         textShape.boundingRectangle.dx,
+         (ctx as any).lineHeight,           // The canvas puts this on as an extra variable
+         true); 
 
       if (shape.isSelected) {
          this.drawSelectionBorder(ctx, shape, IShapeInteractor.defaultGrabHandleDxDy());
